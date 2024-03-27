57927e0e59709e696f6ccee0c60f52093de8ad46
Patrick Bradshaw <patrick.j.bradshaw@gmail.com>
2024-03-27 11:07:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,29 @@ import { RecipesMain } from 'pages/Recipes/Recipes.Main'
 import { RecipeTypeResults } from 'pages/Recipes/RecipeTypeResults'
 import { SearchResults } from 'pages/Recipes/SearchResults'
 import { RegisterPage } from 'pages/Register/RegisterPage'
-// import UserRecipesTable from 'pages/UserRecipes/UserRecipes.Table'
 import { Routes, Route } from 'react-router-dom'
 import ROUTES from 'routes/routes'
 
 export default function App() {
 	return (
-		<>
-			<Routes>
-				<Route path={ROUTES.HOME} element={<HomePage />} />
-				<Route path={ROUTES.LOGIN} element={<LoginPage />} />
-				<Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-				<Route path={ROUTES.DASHBOARD} element={<Dashboard />}>
-					<Route path={ROUTES.MEAL_PLANS} element={<MealPlanTable />} />
-					<Route path={ROUTES.MEAL_PLAN} element={<MealPlanView />} />
-					<Route path={ROUTES.GROCERY_LIST} element={<GroceryListTable />} />
-				</Route>
-				<Route path={ROUTES.RECIPES} element={<RecipesMain />} />
-				<Route path={ROUTES.RECIPE} element={<RecipeDetails />} />
-				<Route path={ROUTES.RECIPE_TYPE_VIEW} element={<RecipeTypeResults />} />
-				<Route path={ROUTES.SEARCH} element={<SearchResults />} />
-				<Route path={ROUTES.HOW_IT_WORKS} element={<HowItWorksPage />} />
+		<Routes>
+			<Route path={ROUTES.HOME} element={<HomePage />} />
+			<Route path={ROUTES.LOGIN} element={<LoginPage />} />
+			<Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+			<Route path={ROUTES.DASHBOARD} element={<Dashboard />}>
+				<Route path={ROUTES.MEAL_PLANS} element={<MealPlanTable />} />
+				<Route path={ROUTES.MEAL_PLAN} element={<MealPlanView />} />
+				<Route path={ROUTES.GROCERY_LIST} element={<GroceryListTable />} />
+			</Route>
+			<Route path={ROUTES.RECIPES} element={<RecipesMain />} />
+			<Route path={ROUTES.RECIPE} element={<RecipeDetails />} />
+			<Route path={ROUTES.RECIPE_TYPE_VIEW} element={<RecipeTypeResults />} />
+			<Route path={ROUTES.SEARCH} element={<SearchResults />} />
+			<Route path={ROUTES.HOW_IT_WORKS} element={<HowItWorksPage />} />
 
-				<Route path={ROUTES.ERROR} element={<ErrorPage />} />
+			<Route path={ROUTES.ERROR} element={<ErrorPage />} />
 
-				<Route path={'*'} element={<ErrorPage />} />
-			</Routes>
-		</>
+			<Route path={'*'} element={<ErrorPage />} />
+		</Routes>
 	)
 }
